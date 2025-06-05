440266b347d3df848005f1865314706a04b0cae6
Vladimir Chernov <vladimir.chernov@rovio.com>
2025-06-05 17:57:36 +0300

    Add empty project for future personal site

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+const App = () => {
+    return (
+        <Container>
+            <Typography>Welcome to my personal site.</Typography>
+        </Container>
+    );
+};
+
+export default App;
